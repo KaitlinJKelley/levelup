053d01898d0dcb2d255a22a2fe59fab702b800e7
kjk1325e <kaitlinjkelley@gmail.comm>
2021-05-06 15:02:51 -0500

    Added event viewset and registered route in url.py

--- levelupapi/models/event.py
+++ levelupapi/models/event.py
@@ -1,14 +1,15 @@
 from django.db import models
-import datetime
+from django.utils import timezone
 from django.db.models.deletion import CASCADE, DO_NOTHING
 from django.db.models.fields.related import ForeignKey
 
 class Event(models.Model):
     event_date = models.DateTimeField()
-    date_created = models.DateTimeField()
-    event_address = models.CharField(max_length=100)
+    time = models.TimeField(default="12:00")
+    date_created = models.DateTimeField(default=timezone.now)
     game = ForeignKey("Game", on_delete=DO_NOTHING, null=True)
-    gamer = ForeignKey("Gamer", on_delete=CASCADE)
+    organizer = ForeignKey("Gamer", on_delete=CASCADE)
+    description = models.CharField(max_length=200,default="")
     # Allows model to grab all attendees; no need for loops,
     # And don't need to JOIN therough SQL
     # related_name will be an attribute on Gamer containing all events
