921205eb3fa389cf2ffc81ef161c3d4d1220aaf1
kjk1325e <kaitlinjkelley@gmail.comm>
2021-05-25 15:09:34 -0500

    Added tests directory, setup GameTests, tested and passed test_create_game

---
+++ tests/__init__.py
@@ -0,0 +1,5 @@
+# from .events_tests import EventTests
+from .game_tests import GameTests
+# from .gamer_tests import GamerTests
+# from .gametype_tests import GameTypeTests
+# from .profile_tests import ProfileTests
